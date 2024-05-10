82471e1a452518431f8f78f1034cdac4f4a308b6
JustYoussef <167704295+HellD4nGer@users.noreply.github.com>
2024-05-10 18:36:55 +0300

    removed unused file

--- component/Cart.tsx
+++ component/Cart.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text } from "react-native";
 import destinationsData from "@/data/destinations.json";
 import AddToCartButton from "./CartButton";
 
