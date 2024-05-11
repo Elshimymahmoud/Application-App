e840be1cfb354b6d85569d356f5078dddbea5978
JustYoussef <167704295+HellD4nGer@users.noreply.github.com>
2024-05-11 04:12:00 +0300

    add signin

---
+++ Firebase/auth.js
@@ -0,0 +1,55 @@
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { app, db } from "./config";
+import { doc, setDoc } from "firebase/firestore";
+
+export const auth = getAuth(app);
+
+export const signUp = async (email, password) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const docRef = doc(db, "users", userCredential.user.uid);
+
+    await setDoc(docRef, {
+      email,
+      cart: [],
+    });
+    console.log(userCredential);
+  } catch (err) {
+    console.log(err);
+  }
+
+  // createUserWithEmailAndPassword(auth, email, password)
+  //     .then(userCredential => console.log(userCredential))
+  //     .catch(err => console.log(err))
+};
+
+export const signIn = async (email, password) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    console.log(userCredential);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const signOutAsync = async () => {
+  try {
+    await signOut(auth);
+  } catch (err) {
+    console.log(err);
+  }
+};
--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -1,10 +1,72 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import { signIn } from "../../Firebase/auth";
+import { useNavigate } from "react-router-native";
 
-export default function login() {
+const SignInScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [count, setCount] = useState(0);
+  const navigate = useNavigate();
+
   return (
-    <View>
-      <Text>login</Text>
+    <View style={styles.container}>
+      <Text>{count}</Text>
+      <TextInput
+        placeholder="Enter your email"
+        style={styles.textInput}
+        value={email}
+        onChangeText={(e) => setEmail(e)}
+        keyboardType="email-address"
+      />
+      <TextInput
+        placeholder="Enter your password"
+        style={styles.textInput}
+        value={password}
+        onChangeText={(e) => setPassword(e)}
+        secureTextEntry={true}
+      />
+      <View style={{ alignItems: "center" }}>
+        <Pressable
+          style={styles.btn}
+          onPress={async () => {
+            await signIn(email, password);
+          }}
+        >
+          <Text>Sign in</Text>
+        </Pressable>
+        <Pressable
+          style={styles.btn}
+          onPress={() => {
+            navigate("/signUp");
+          }}
+        >
+          <Text>Sign up</Text>
+        </Pressable>
+      </View>
     </View>
   );
-}
+};
+
+export default SignInScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  textInput: {
+    padding: 8,
+    borderRadius: 16,
+    borderWidth: 1,
+    borderColor: "#d3d3d3",
+    marginVertical: 8,
+  },
+  btn: {
+    padding: 8,
+    borderRadius: 16,
+    elevation: 5,
+    backgroundColor: "cyan",
+    marginVertical: 8,
+    alignItems: "center",
+  },
+});
